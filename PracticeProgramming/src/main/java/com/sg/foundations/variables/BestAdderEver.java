90fbb8191ff0cce748a5cc2742b29daa5ba5bbc3
Abdul Rizvi <kh_abdul@hotmail.co.uk>
2022-02-16 12:10:44 +0000

    Best Adder file

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/BestAdderEver.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author Frizbeee
+ */
+public class BestAdderEver {
+    
+    public static void main(String[] args) {
+        float num1;
+        float num2;
+        float num3;
+        float total;
+        
+        
+        num1 = 3.1415f;
+        num2 = 420;
+        num3 = 2.3f;
+        
+        total = num1 + num2 + num3;
+        
+        System.out.print("The sum is: " + total);
+        
+        
+        
+    }
+    
+}
